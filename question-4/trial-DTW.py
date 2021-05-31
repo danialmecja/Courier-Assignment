70ca8aa9ff46a891e58eee2e50127d86af28a14b
Danial Mecja <danialmecja@gmail.com>
2021-05-31 22:26:03 +0800

    Update trial DTW file to reflect overlapping audio files and calculate distance of each file with base clip

--- question-4/trial-DTW.py
+++ question-4/trial-DTW.py
@@ -7,23 +7,29 @@ from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 
 # Read audio files in folder
-fs1, data1 = wavfile.read("question-4/Example1.wav")
-fs2, data2 = wavfile.read("question-4/Example2.wav")
-fs3, data3 = wavfile.read("question-4/irrelevantWAVExample.wav")
+fs1, data1 = wavfile.read("question-4/kami_ingin_memohon_maaf.wav")
+fs2, data2 = wavfile.read("question-4/Hafizthingy.wav")
+fs3, data3 = wavfile.read("question-4/Hafizthingydifferent.wav")
+fs4, data4 = wavfile.read("question-4/IguessthatsokayHafiz.wav")
 
 # Taking max values along axis
 data1 = np.amax(data1, axis=1)
 data2 = np.amax(data2, axis=1)
 data3 = np.amax(data3, axis=1)
+data4 = np.amax(data4, axis=1)
 
 # Plot style
 plt.style.use('seaborn-whitegrid')
 
 print(fastdtw(data1, data2)[0])
+print(fastdtw(data1, data3)[0])
+print(fastdtw(data1, data4)[0])
 
 # create subplots
 ax = plt.subplot(1, 1, 1)
-ax.plot(data1, color=("#67A0DA"))
+ax.plot(data1, color=("#67A0DA"), alpha=0.5)
 ax.plot(data2, color="#DAA067", alpha=0.5)
+ax.plot(data3, color='#DADA67', alpha=0.5)
+ax.plot(data4, color='#3FBF7F', alpha=0.5)
 fig = plt.show()
 display(fig)
