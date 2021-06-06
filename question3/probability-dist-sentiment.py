e3a2bedf94af626f04c16c367687cdad81ff4892
Jeevan <jeevanpillai31@gmail.com>
2021-06-07 05:19:13 +0800

    implemented new scoring algo

--- question3/probability-dist-sentiment.py
+++ question3/probability-dist-sentiment.py
@@ -1,64 +1,51 @@
 import matplotlib
-from ..question1.courier_q1 import results
-
-import sys, os
-
-
 
 # 'courier' : [positivity, negativity]
 couriers = {'DHL': [0.661, 0.339],
              'Pos Laju': [0.61, 0.39], 
-             'NinjaVan': [0.804, 0.196], 
-             'JNT': [0.636, 0.364], 
-             'GDex': [0.711, 0.289]}
+             'Ninja-Van': [0.804, 0.196], 
+             'J&T': [0.636, 0.364], 
+             'GDEX': [0.711, 0.289]}
 
-distances = {"Customer 1" : 
-                    {'DHL' : 55.94, 
-                    'Pos Laju' : 63.24,
-                    'NinjaVan' : 99.03,
-                    'JNT' : 124.95,
-                    'GDex' : 61.01},
-            "Customer 2" : 
-                    {'DHL' : 47.52, 
-                    'Pos Laju' : 55.02,
-                    'NinjaVan' : 70.25,
-                    'JNT' : 105.96,
-                    'GDex' : 77.02},
-            "Customer 3" : 
-                    {'DHL' : 83.51, 
-                    'Pos Laju' : 53.56,
-                    'NinjaVan' : 111.776,
-                    'JNT' : 52.39,
-                    'GDex' : 72.43}
+distances = {
+            "Customer 1":{"DHL":53.944, "GDEX":61.078, "Pos Laju":63.230, "Ninja-Van":99.022, "J&T":124.942},
+            "Customer 2":{"DHL":47.523, "Pos Laju":56.006, "Ninja-Van":70.249, "GDEX":77.009, "J&T":105.959},
+            "Customer 3":{"J&T":52.392, "Pos Laju":54.168, "GDEX":72.422, "DHL":83.508, "Ninja-Van":111.776}
             }
 
-pd = {}
+print("Weighted score = (1 - dist_x/dist_sum) * positvity")
+
+pd = {
+    "Customer 1":{},
+    "Customer 2":{},
+    "Customer 3":{}
+    }
 
 for cust in distances:
-    min = 9999
-    pd[cust] = {}
-    for courier in distances.get(cust):
-        d = distances.get(cust).get(courier)
-        pd[cust][courier] = round(d - d * couriers.get(courier)[0], 2)
-        if (d < min):
-            min = d
-            min_courier = courier
+    sum = 0
+    for cour in distances.get(cust):
+        sum+=distances.get(cust).get(cour)
 
+    for cour in distances.get(cust):
+        pd[cust][cour] = [round(1 - distances.get(cust).get(cour)/sum, 4), round((1 - distances.get(cust).get(cour)/sum) * couriers.get(cour)[0],4)]
+        ## stores for each courier for each customer an array with 2 values: 
+        # [0] is the distance score 
+        # [1] is the weighted score
 
-print("Weighted score = distance - (distance * positivity)\n")
 
 for cust in pd:
-    min_pd = 999
-    min_dist = 999
-    print("For %s, weighted score of distance and positivity are:" % (cust))
-    for courier in pd.get(cust):
-        print(" - %s = %.2f" % (courier,  pd.get(cust).get(courier)))
-        if (pd.get(cust).get(courier) < min_pd):
-            min_pd = pd.get(cust).get(courier)
-            min__pd_courier = courier
-        if (distances.get(cust).get(courier) < min_dist):
-            min_dist = distances.get(cust).get(courier)
-            min__dist_courier = courier
-    print("\nMinimum Probability -> %s with score of %.2f" % (min__pd_courier,min_pd))
-    print("Minimum Distance    -> %s with score of %.2f\n\n" % (min__dist_courier,min_dist))
+    best_score = 0
+    best_hub = ""
+    print(cust)
+    for cour in pd.get(cust):
+        print("  %s \n  - Distance Score: %.4f\n  - Weighted Score: %.4f"%(cour, pd.get(cust).get(cour)[0], pd.get(cust).get(cour)[1] ))
+        if(pd.get(cust).get(cour)[1] > best_score):
+            best_score = pd.get(cust).get(cour)[1]
+            best_hub = cour
 
+
+    print("\n-Best Weighted Score: %.4f via %s" % (best_score, best_hub))
+    print("-Best Distance Score: %.4f via %s\n" % (list(pd.get(cust).values())[0][0], list(pd.get(cust).keys())[0]))
+
+
+    
